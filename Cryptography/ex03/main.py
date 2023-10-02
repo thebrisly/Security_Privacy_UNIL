1c4bcf6b0793f0cb0377b0906155a8d9be06f24a
Laura hp <laura.fabbiano.gomez@hotmail.com>
2023-10-02 20:44:42 +0200

    added more documentation for ex03

--- Cryptography/ex03/main.py
+++ Cryptography/ex03/main.py
@@ -9,6 +9,9 @@ key_file.close()
 
 
 # TODO open the encrypted image
+# La fonction open() renvoie un objet file qui peut être utilisé pour lire, écrire et modifier le fichier.
+# La fonction read() est utilisée pour lire le contenu d'un fichier déjà ouvert et stocke le contenu dans la variable (ici encrypted_image)
+# Une fois que l'on a lu l'image et stocké le contenu qqpart on peut fermer le fichier de référence.
 image_file = open("encrypted_image", "rb")
 encrypted_image = image_file.read()
 image_file.close()
@@ -18,12 +21,12 @@ image_file.close()
 # The image was encrypted using AES encryption in the Electronic CodeBook mode of operation
 # https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/
 
-decrypted_image = Cipher(algorithms.AES(key), modes.ECB())
-decryptor = decrypted_image.decryptor()
+# Decryptor est utilisé pour effectuer réellement le processus de déchiffrement des données.
+decrypted_image = (Cipher(algorithms.AES(key), modes.ECB())).decryptor()
 
 
 # TODO save the decrypted image in the image binary variable.
-image_binary = decryptor.update(encrypted_image) + decryptor.finalize()
+image_binary = decrypted_image.update(encrypted_image) + decrypted_image.finalize()
 
 
 # TODO Save the image in jpg format
