2846d269e37a09fbb5415142cce23fd925856bb7
Laura hp <laura.fabbiano.gomez@hotmail.com>
2023-10-02 22:06:13 +0200

    reorganization - Cryptography repo

---
+++ Cryptography/ex06/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+This is the starting file for doing Exercise 6.
+"""
+
+hashed_phone_numbers = {
+    "participants": [
+        {
+            "work phone": "4c48cb6ac1fca984ff41bcd1888d01e5",
+            "mobile phone": "aa40f062b5b7e40e139faf7f9ef8f5e6",
+            "contacts": ["044768b20fc6cce49d9ab13a8ebc6ae9", "364600351ad65479697bd06b714a649c", "1f5c308421c857a7fe32a878ddb20967"
+            ]
+        },
+        {
+            "work phone": "785f7d94d4bfceb1e954b724cf01d4c0",
+            "mobile phone": "64939f75ab752856a75fcfd731baf8ea",
+            "contacts": ["3bc47f97d5b7805af2f75bf28b4334e4", "25e1577a6534156615bbe17a37798316"
+            ]
+        }
+    ]
+}
+
+
+if __name__ == "__main__":
+    pass
